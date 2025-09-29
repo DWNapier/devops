7565b10ec214fd2c9379a8be95c4504fd08142e3
Dave Watkin <40803890@live.napier.ac.uk>
2025-09-29 19:44:26 +0100

    Added department and manager to getEmployee query.

--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -99,10 +99,14 @@ public class App
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT e.emp_no, e.first_name, e.last_name, t.title, s.salary "
+                    "SELECT e.emp_no, e.first_name, e.last_name, t.title, s.salary, d.dept_name, m.first_name, m.last_name "
                     + "FROM employees e "
                     + "JOIN titles t ON e.emp_no = t.emp_no AND t.to_date = '9999-01-01' "
                     + "JOIN salaries s ON e.emp_no = s.emp_no AND s.to_date = '9999-01-01' "
+                    + "JOIN dept_emp de ON e.emp_no = de.emp_no AND de.to_date = '9999-01-01' "
+                    + "JOIN departments d ON de.dept_no = d.dept_no "
+                    + "JOIN dept_manager dm ON de.dept_no = dm.dept_no AND dm.to_date = '9999-01-01' "
+                    + "JOIN employees m ON dm.emp_no = m.emp_no "
                     + "WHERE e.emp_no = " + ID;
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
@@ -116,6 +120,10 @@ public class App
                 emp.last_name = rset.getString("e.last_name");
                 emp.title = rset.getString("t.title");
                 emp.salary = rset.getInt("s.salary");
+                emp.dept_name = rset.getString("d.dept_name");
+                emp.manager = rset.getString("m.first_name");
+                emp.manager += (" ");
+                emp.manager += rset.getString("m.last_name");
                 return emp;
             }
             else
